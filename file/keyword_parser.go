4bc5faef9e3f4516a604d01c3482b47c6149562e
zhangjin <zhangjin@yunzhangfang.com>
2020-06-24 17:11:59 +0800

    add log foramte

--- file/keyword_parser.go
+++ file/keyword_parser.go
@@ -1,2 +1,29 @@
 package file
 
+
+var keyWordArr = []string{"using","rand()", "union", "select *", "*"}
+
+//关键词的内容提示
+func KeyWord(fileMap map[int]string, fileName string) []LogInfo {
+	logList := []LogInfo{}
+	if len(fileMap) == 0{
+		return logList
+	}
+
+	for k, v := range fileMap{
+		if c,keyword :=contains(keyWordArr, v); c {
+			logList = append(logList, LogInfo{FileName:fileName, location:string(k), msg:"存在关键字"+ keyword})
+		}
+	}
+
+	return logList
+}
+
+func contains(a []string, x string) (bool, string)  {
+	for _, n := range a {
+		if x == n {
+			return true,x
+		}
+	}
+	return false,x
+}
--- file/sql_analyze.go
+++ file/sql_analyze.go
@@ -3,4 +3,16 @@ package file
 /**
  * *解析出来的异常信息
  */
-var errorMsg = []string{}
+var errorMsgList = []LogInfo{}
+
+//日志内容
+type LogInfo struct {
+	FileName string
+	location string
+	msg string
+}
+
+//日志格式，是不是格式都放在这里维护
+func (log *LogInfo) FormatLog() string {
+	return "文件: " + log.FileName + log.msg
+}
